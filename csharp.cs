c3cb67afee5998615d530f8e5942cbc31730d476
Jeffrey Graham Sanchez Perez <jesanchez@contraloria.com>
2016-01-15 14:39:55 -0400

    First commit

---
+++ csharp.cs
@@ -0,0 +1,13 @@
+using Systen;
+namespace csharp
+	{
+	class csharps
+		{
+	public static void Main(string[] args)
+         {
+ 	  Console.WriteLine("Hello World");
+	  Console.ReadKey();
+			}
+		}
+	}
+	
